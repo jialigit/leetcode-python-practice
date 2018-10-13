b9f06baaf862da495071657cb61be3960e9231bd
jialigit <1392029926@qq.com>
2018-10-13 22:11:54 +0800

    prime function def

--- cal_prime.py
+++ cal_prime.py
@@ -1,10 +1,29 @@
-#coding:utf-8
+#! /bin/env python
+#-*-coding:utf-8
 
 #this is the function to calculate the primes less than n
-def cal_prime(n):
-  pass
-  
-cal_prime(10)
+import math
+import sys
+ 
+def prime(n):
+    if n <= 1:
+        return 0
+    #for i in range(2,int(math.sqrt(n)+1)):
+    for i in range(2,n):
+        if n%i == 0:
+            return 0
+    return 1
+
+ 
+
+if __name__ == "__main__":
+	n = int(sys.argv[1])
+
+	for i in range(2,n+1):
+		if prime(i):
+ 			print(i)
+
+
 
 
 
